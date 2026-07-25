b75c5676a5cdd6cf635fc606a8b2b00bb85fa2fe
Joseph Corella <josephc8464@gmail.com>
2026-07-24 21:06:01 -0700

    Commented out boardcontext

--- scenes/game_scene.py
+++ scenes/game_scene.py
@@ -5,7 +5,4 @@ class GameScene():
     def __init__(self):
         self.placeholder = 0
         self.ui = UIRenderer()
-        self.board_renderer = DefaultBoardRenderer()
-
-    def draw():
-        self.board_renderer.render_board()
+        #self.board_renderer = DefaultBoardRenderer()
